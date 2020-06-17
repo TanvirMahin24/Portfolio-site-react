9ff47142ecb3a07228b3d7746bb67d5d561f837f
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2020-06-18 02:31:07 +0600

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -1,7 +1,9 @@
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://tanvirmahin.herokuapp.com/' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://tanvirmahin.herokuapp.com/',
-  'process.env.CLIENT_ID': '6ZXORIFR1F4iiw5QhR2wZxcYjiUEbd6F'
-}
+  "process.env.BASE_URL": prod
+    ? "https://tanvirmahin.herokuapp.com"
+    : "http://localhost:3000",
+  "process.env.NAMESPACE": "https://tanvirmahin.herokuapp.com",
+  "process.env.CLIENT_ID": "6ZXORIFR1F4iiw5QhR2wZxcYjiUEbd6F",
+};
